70ed8078ee2104456b1bf4e2a452708d438433b4
Neevs1 <neevan.redkar@gmail.com>
2024-07-23 14:43:04 +0530

    Added insertion sort

--- Assignment1/studentDatabase.cpp
+++ Assignment1/studentDatabase.cpp
@@ -8,6 +8,7 @@ struct Student{
 };
 
 void bsort(struct Student[]);
+void inSort(struct Student[]);
 
 int main(){
     struct Student se_it[15]={{15,"X",8.77},{12,"P",9.54},{13,"Q",6.22},{14,"W",7.55},{5,"E",9.23},
@@ -15,6 +16,7 @@ int main(){
     {11,"O",8.90},{1,"A",7.25},{3,"S",8.04},{4,"D",7.79},{2,"F",9.33}};
 
     bsort(se_it);
+    inSort(se_it);
 
     return 0;
 }
@@ -37,6 +39,39 @@ void bsort(struct Student arr[]){
     }
     cout<<"List sorted by roll number is"<<endl;
     for(int i=0;i<15;i++){
-        cout<<arr[i].rollNum<<" "<<arr[i].name<<" "<<arr[i].sgpa<<"\n";
+        cout<<arr[i].rollNum<<" "<<arr[i].name<<" "<<arr[i].sgpa<<endl;
+    }
+}
+
+void inSort(struct Student arr[]){
+    int j,keyr;
+    float keys;
+    string key;
+    for(int i=1;i<15;i++){
+        key=arr[i].name;
+        keyr=arr[i].rollNum;
+        keys=arr[i].sgpa;
+        j=i-1;
+        //cout<<key<<" "<<j<<endl;
+        while(j>=0){
+            if(arr[j].name<key){
+                break;
+            }else{
+                arr[j+1].name=arr[j].name;
+                arr[j].name=key;
+                arr[j+1].rollNum=arr[j].rollNum;
+                arr[j].rollNum=keyr;
+                arr[j+1].sgpa=arr[j].sgpa;
+                arr[j].sgpa=keys;
+                j--;
+            }
+        }
+            
+    }
+    cout<<"Sorted alphabetically"<<endl;
+    for(int i =0;i<15;i++){
+        
+        cout<<arr[i].rollNum<<" "<<arr[i].name<<" "<<arr[i].sgpa;
+        cout<<endl;
     }
 }
