610db291ef797fcc4efd6e4cc88374ea53408266
Neevs1 <78549886+Neevs1@users.noreply.github.com>
2024-07-25 11:46:36 +0530

    added function to calculate toppers

--- Assignment1/studentDatabase.cpp
+++ Assignment1/studentDatabase.cpp
@@ -7,8 +7,8 @@ struct Student{
     float sgpa;
 };
 
-void bsort(struct Student[]);
-void inSort(struct Student[]);
+void bsort(struct Student[],int);
+void inSort(struct Student[],int);
 void qSort(struct Student[],int,int);
 int partition(struct Student[],int,int);
 
@@ -16,72 +16,77 @@ int main(){
     struct Student se_it[15]={{15,"X",8.77},{12,"P",9.54},{13,"Q",6.22},{14,"W",7.55},{5,"E",9.23},
     {6,"R",5.40},{10,"T",8.77},{8,"Y",5.68},{7,"U",9.18},{9,"I",7.89},
     {11,"O",8.90},{1,"A",7.25},{3,"S",8.04},{4,"D",7.79},{2,"F",9.33}};
+    int length = sizeof(se_it)/sizeof(se_it[0]);
+    
 
-    bsort(se_it);
-    inSort(se_it);
+    bsort(se_it,length-1);
+    cout<<endl;
+    inSort(se_it,length-1);
+    cout<<endl;
+    qSort(se_it,0,length-1);
+    cout<<"10 toppers in classroom are"<<endl;
+    for(int i=14;i>4;i--){
+        cout<<se_it[i].rollNum<<" "<<se_it[i].name<<" "<<se_it[i].sgpa<<endl;
+        
+    }
 
     return 0;
 }
 
-void bsort(struct Student arr[]){
-   for(int i =0;i<15;i++){
-        for(int j=0;j<15;j++){
+void bsort(Student  arr[],int length){
+    int temp;
+    string tmp;
+    float t;
+   for(int i =0;i<length;i++){
+        for(int j=0;j<length;j++){
             if(arr[j].rollNum>arr[j+1].rollNum){
-                int temp = arr[j].rollNum;
-                arr[j].rollNum = arr[j+1].rollNum;
-                arr[j+1].rollNum= temp;
-                string tmp = arr[j].name;
-                arr[j].name = arr[j+1].name;
-                arr[j+1].name= tmp;
-                float t = arr[j].sgpa;
-                arr[j].sgpa = arr[j+1].sgpa;
-                arr[j+1].sgpa= t;
+                swap(arr[j+1],arr[j]);              
             }
+
         }
     }
     cout<<"List sorted by roll number is"<<endl;
-    for(int i=0;i<15;i++){
-        cout<<arr[i].rollNum<<" "<<arr[i].name<<" "<<arr[i].sgpa<<endl;
+    for(int i=0;i<length+1;i++){
+        cout<<arr[i].rollNum<<" "<<arr[i].name<<" "<<arr[i].sgpa;
+        cout<<"\n";
     }
 }
 
-void inSort(struct Student arr[]){
-    int j,keyr;
-    float keys;
-    string key;
-    for(int i=1;i<15;i++){
-        key=arr[i].name;
-        keyr=arr[i].rollNum;
-        keys=arr[i].sgpa;
+
+
+void inSort(struct Student arr[],int length)
+{
+    int j;//,keyr;
+   // float keys;
+    //string key;
+    for(int i=1;i<length+1;i++){
+        Student key=arr[i];
+       /* keyr=arr[i].rollNum;
+        keys=arr[i].sgpa;*/
         j=i-1;
         while(j>=0){
-            if(arr[j].name<key){
+            if(arr[j].name<key.name){
                 break;
             }else{
                 arr[j+1].name=arr[j].name;
-                arr[j].name=key;
+                arr[j].name=key.name;
                 arr[j+1].rollNum=arr[j].rollNum;
-                arr[j].rollNum=keyr;
+                arr[j].rollNum=key.rollNum;
                 arr[j+1].sgpa=arr[j].sgpa;
-                arr[j].sgpa=keys;
+                arr[j].sgpa=key.sgpa;
                 j--;
             }
         }
             
     }
-    cout<<"Sorted alphabetically"<<endl;
+    std::cout<<"Sorted alphabetically"<<endl;
     for(int i =0;i<15;i++){
         
-        cout<<arr[i].rollNum<<" "<<arr[i].name<<" "<<arr[i].sgpa;
-        cout<<endl;
+        std::cout<<arr[i].rollNum<<" "<<arr[i].name<<" "<<arr[i].sgpa;
+        std::cout<<endl;
     }
 }
 
-void swap(int *a, int *b){
-    int temp = *a;
-    *a =*b;
-    *b = temp;
-}
 
 int partition(Student arr[],int low,int high){
     float pivot = arr[high].sgpa;
@@ -89,10 +94,11 @@ int partition(Student arr[],int low,int high){
     for(int j = low;j<high;j++){
         if(arr[j].sgpa<=pivot){
             i++;
-            swap(&arr[i],&arr[j]);
+            swap(arr[i],arr[j]);
+            
         }
     }
-    swap(&arr[i+1],&arr[high]);
+    swap(arr[i+1],arr[high]);
     return (i+1);
 }
 
