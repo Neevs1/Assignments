c04015786613067b2c87e126b1c862919d7c0e9a
Neevs1 <neevan.redkar@gmail.com>
2024-07-26 21:43:40 +0530

    Added comments, cosmetic changes

--- Assignment1/studentDatabase.cpp
+++ Assignment1/studentDatabase.cpp
@@ -1,19 +1,29 @@
+/*
+  Name - Neevan Redkar
+  Roll no - 3069 
+  CRN - 23103069
+  Class - SE
+  Batch S3
+*/
+
+
 #include <iostream>
 using namespace std;
 
-struct Student{
+struct Student{  //declared struct Student 
     int rollNum;
     string name;
     float sgpa;
 };
 
-void bsort(struct Student[],int);
+void bsort(struct Student[],int);  
 void inSort(struct Student[],int);
 void qSort(struct Student[],int,int);
 int partition(struct Student[],int,int);
 void gpasearch(struct Student[],int);
 int namesearch(struct Student[],int,int);
 void print(struct Student[],int);
+//declared all required fucntions
 
 int main(){
     struct Student se_it[15]={{15,"X",8.77},{12,"P",9.54},{13,"Q",6.22},{14,"W",7.55},{5,"E",9.23},
@@ -21,28 +31,28 @@ int main(){
     {11,"O",8.90},{1,"A",7.25},{3,"S",8.04},{4,"D",7.79},{2,"F",9.33}};
     //GPA sourced from random number generator online
     int length = sizeof(se_it)/sizeof(se_it[0]);
-    
+    //int length using sizeof() inbuilt function    
 
-    bsort(se_it,length-1);
+    bsort(se_it,length-1); //called bubble sort function to sort by roll numbers
     
-    inSort(se_it,length-1);
-    cout<<"Sorted alphabetically";
-    print(se_it,length);
-    qSort(se_it,0,length-1);
-    cout<<"10 toppers in classroom are"<<endl;
-    for(int i=14;i>4;i--){
+    inSort(se_it,length-1); //sorted array by alphabetical order of name of Students
+    cout<<"Sorted alphabetically"<<endl;
+    print(se_it,length); //printed array
+    qSort(se_it,0,length-1); //Quick sorted array to calculate toppers
+    cout<<"10 toppers in classroom are"<<endl;  
+    for(int i=14;i>4;i--){ //as array is in ascending order,print from other end
         cout<<se_it[i].rollNum<<" "<<se_it[i].name<<" "<<se_it[i].sgpa<<endl;
         
     }
     cout<<endl;
-    gpasearch(se_it,length);
-    int result = namesearch(se_it,length-1,0);
+    gpasearch(se_it,length); //linear search for CGPA
+    int result = namesearch(se_it,length-1,0); 
     (result==-1)?cout<<"Given name is not in array"<<endl: cout<<"Given name is present at array having roll number "<<se_it[result].rollNum<<endl;
-
+    //Used ternary operator to see whether name is in array. If condition met, roll no. of student returned
     return 0;
 }
 
-void print(Student arr[],int length){
+void print(Student arr[],int length){ //function for printing
    for(int i=0;i<length;i++){
         cout<<arr[i].rollNum<<" "<<arr[i].name<<" "<<arr[i].sgpa;
         cout<<"\n";
@@ -50,14 +60,14 @@ void print(Student arr[],int length){
     cout<<endl;
 }
 
-void bsort(Student  arr[],int length){
+void bsort(Student  arr[],int length){ //bubble sort function
     int temp;
     string tmp;
     float t;
    for(int i =0;i<length;i++){
         for(int j=0;j<length;j++){
             if(arr[j].rollNum>arr[j+1].rollNum){
-                swap(arr[j+1],arr[j]);              
+                swap(arr[j+1],arr[j]);    //using in-built swap function in iostream std.          
             }
 
         }
@@ -72,19 +82,19 @@ void inSort(struct Student arr[],int length)
 {
     int j;
     for(int i=1;i<length;i++){
-        Student key=arr[i];
+        Student key=arr[i]; //took a seperate key
         j=i-1;
         while(j>=0){
             if(arr[j].name<key.name){
-                break;
+                break; //ignore if greater
             }else{
-                arr[j+1].name=arr[j].name;
+                arr[j+1].name=arr[j].name; 
                 arr[j].name=key.name;
                 arr[j+1].rollNum=arr[j].rollNum;
                 arr[j].rollNum=key.rollNum;
                 arr[j+1].sgpa=arr[j].sgpa;
                 arr[j].sgpa=key.sgpa;
-                j--;
+                j--; //replace with key if smaller
             }
         }
             
@@ -94,7 +104,7 @@ void inSort(struct Student arr[],int length)
 }
 
 
-int partition(Student arr[],int low,int high){
+int partition(Student arr[],int low,int high){ //pivot & swap 
     float pivot = arr[high].sgpa;
     int i = low-1;
     for(int j = low;j<high;j++){
@@ -108,7 +118,7 @@ int partition(Student arr[],int low,int high){
     return (i+1);
 }
 
-void qSort(struct Student arr[],int low,int high){
+void qSort(struct Student arr[],int low,int high){ //recursive quick sort
     if (low < high) {
     
     int pi = partition(arr, low, high);
@@ -119,39 +129,39 @@ void qSort(struct Student arr[],int low,int high){
 
 void gpasearch(Student arr[],int length){
     float target;
-    int counter=-1;
+    int counter=-1; //counter initialized with -1
     cout<<"Enter CGPA of student to be found ";
-    cin>>target;
-    for(int i=0;i<length;i++){
+    cin>>target;//input of target
+    for(int i=0;i<length;i++){ //linear search
         if(target==arr[i].sgpa){
             cout<<"Student having CGPA "<<target<<" is "<<arr[i].name<<" and their roll no is "<<arr[i].rollNum<<endl;
-            counter++;
+            counter++;//counter increased if condition met
         }
     }
-    if(counter<0){
+    if(counter<0){  //if element not found, counter negative
         cout<<"No student having CGPA "<<target<<" found"<<endl;
     }
 
 }
 
 int namesearch(Student arr[],int high,int low){
-  inSort(arr,high);
-  string x;
+  inSort(arr,high); //called insertion sort function defined above, to sort in ascending order of name
+  string target; 
   cout<<"Enter name to be found"<<endl;
-  cin>>x;
-  while (low <= high) {
+  cin>>target; //input of target element
+  while (low <= high) { //Binary search by implementing while loop, divides array
         int mid = low + (high - low) / 2;
 
-        if (arr[mid].name == x){
-            return mid;
+        if (arr[mid].name == target){
+            return mid; //returns if found
         }
-        else if(arr[mid].name < x){
-             low = mid + 1;
+        else if(arr[mid].name < target){
+             low = mid + 1; //lower part of array
         }else{
-            high = mid - 1;}
+            high = mid - 1;} //takes upper part of array
     }
 
 
-    return -1;
+    return -1; //returns -1 if element not found
 
 }
