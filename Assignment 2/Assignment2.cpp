d1d68b5c1632ca352a0fe5fe13183df28bec94a6
Neevs1 <neevan.redkar@gmail.com>
2024-08-20 14:55:47 +0530

    Added condition to append stack elements at the end

--- Assignment 2/Assignment2.cpp
+++ Assignment 2/Assignment2.cpp
@@ -22,19 +22,26 @@ node* createNode(string input){
   return new_node;    
 }
 
-string in2post();
+struct Stack{
+      node* head;
+};
+
+
+
+string in2post(Stack*);
 void in2pre();
 void evalPost();
 void evalPre();
 
 int main(){
+  Stack stack;
   cout<<"Enter choice of operation\n1.Infix to postfix\n2.Infix to Prefix";
   cout<<"\n3.Evaluate postfix\n4.Evaluate prefix";
   int choice;
   cin>>choice;
   switch(choice){
     case 1:
-    cout<<in2post();
+    cout<<in2post(&stack);
     break;
     default:
     cout<<"Option not yet available/invalid input";    
@@ -44,7 +51,15 @@ int main(){
     return 0;
 }
 
-string in2post(){
+void push(Stack* stack,string input){
+  node *new_node = createNode(input);
+  new_node->next=stack->head;
+  stack->head= new_node;
+}
+
+
+
+string in2post(Stack* stack){
   string input;
   string stop,exp;
   bool isNum;
@@ -62,13 +77,15 @@ string in2post(){
       exp.append(input);
       exp.append(" ");
     }else if(input=="+"||input=="-"||input=="*"||input=="/"){
-      createNode(input);
+      push(stack,input);
     }else{
       cout<<"Invalid input";
     }
     cout<<"\nDo you wish to continue?(type 'yes' case sensitive to continue)";
     cin>>stop;
   }while(stop == "yes");
+  if(stack->head->data.empty()==false){
+  exp.append(stack->head->data);}
   return exp;
 
 }
