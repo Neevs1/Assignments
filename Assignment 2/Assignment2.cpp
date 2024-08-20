28db195ff21bb4364b0d60f7f5d85362e22bc2eb
Neevs1 <neevan.redkar@gmail.com>
2024-08-20 13:10:24 +0530

    Added menu

--- Assignment 2/Assignment2.cpp
+++ Assignment 2/Assignment2.cpp
@@ -1,12 +1,22 @@
+/*
+ Name - Neevan Redkar
+ Class - SE
+ Batch S3
+ Roll no 3069
+*/
 #include <iostream>
 using namespace std;
 
-struct node{
+
+
+struct node{ //defined struct
   char data;
-  node* next;
+  node *next;
 };
 
 int main(){
+  cout<<"Enter choice of operation\n1.Infix to postfix\n2.Infix to Prefix";
+  cout<<"\n3.Evaluate postfix\n4.Evaluate prefix";  
 
     return 0;
 }
