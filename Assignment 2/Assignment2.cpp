92c6a619bed6f5745afd78cefb7c3a7ed6c3c145
Neevs1 <neevan.redkar@gmail.com>
2024-08-20 13:41:34 +0530

    Adding Infix to postfix function

--- Assignment 2/Assignment2.cpp
+++ Assignment 2/Assignment2.cpp
@@ -10,13 +10,42 @@ using namespace std;
 
 
 struct node{ //defined struct
-  char data;
-  node *next;
+  string data;
+  struct node *next;
 };
+
+node* createNode(struct node** head,int input){
+  node* new_node = (node*)malloc(sizeof(node));
+  new_node->data = input;
+  new_node->next = NULL;
+  return new_node;    
+}
+
+string in2post();
+void in2pre();
+void evalPost();
+void evalPre();
 
 int main(){
   cout<<"Enter choice of operation\n1.Infix to postfix\n2.Infix to Prefix";
-  cout<<"\n3.Evaluate postfix\n4.Evaluate prefix";  
+  cout<<"\n3.Evaluate postfix\n4.Evaluate prefix";
+  int choice;
+  cin>>choice;
+  switch(choice){
+
+  }  
 
     return 0;
+}
+
+string in2post(){
+  string input;
+  string stop,exp;
+  do{
+    cout<<"Enter element";
+    
+    
+
+  }while(stop != "yes");
+
 }
