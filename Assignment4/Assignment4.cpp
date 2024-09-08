a57f63e9cc7ac956aed0577a4aea9f2dfaf17273
Neevs1 <neevan.redkar@gmail.com>
2024-09-08 21:08:29 +0530

    Added method of building tree and inorder traversal using recursion

--- Assignment4/Assignment4.cpp
+++ Assignment4/Assignment4.cpp
@@ -5,31 +5,61 @@
 using namespace std;
 
 struct node{
-  char data;
-  node* next;
+ char data;
+ 
   node* left;
   node* right;
 };
 
-node* head=NULL;
+
 
 node* createNode(char input){
     node* node = (struct node*)malloc(sizeof(struct node));
     node->data = input;
     node->left = NULL;
     node->right=NULL;
-    node->next=NULL;
+   
     return node;
 }
 
-void push(struct node*){
+node* buildtree(string input){
+  stack<node*> stack;
+  for(int i=0;i<input.length();i++){
+    if(isdigit(input[i])||isalpha(input[i])){
+      node* new_node = createNode(input[i]);
+      stack.push(new_node);
+    }else if(input[i]=='+'||input[i]=='-'||input[i]=='*'||input[i]=='/'){
+      struct node* new_node =createNode(input[i]);
+      new_node->left=(stack.top());
+      stack.pop();
+      new_node->right= (stack.top());
+      stack.pop();
+      stack.push(new_node);
 
+    }
+
+  }
+  return stack.top();  
+  
 }
+
+void inorder(struct node* root) {
+    if (root) {
+        inorder(root->left);
+        cout<<(root->data);
+        inorder(root->right);
+    }
+}
+
+
 
 int main(){
    string input;
    cout<<"Enter input \n";
    cin>>input;
+   node* tree = buildtree(input);
+   inorder(tree);
+   
 
 
     return 0;
