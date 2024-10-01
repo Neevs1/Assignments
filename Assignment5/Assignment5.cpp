3da812787b2bcc1decacef18f342852e968fd2ca
Neevs1 <neevan.redkar@gmail.com>
2024-10-01 12:52:56 +0530

    Added all features+printout

--- Assignment5/Assignment5.cpp
+++ Assignment5/Assignment5.cpp
@@ -34,6 +34,93 @@ struct node* insert(node* Node,int data){
     return Node;    
 }
 
+void search(node* Node,int data){
+    if(Node == NULL){
+        cout<<"Value not found"<<endl;
+    }else if(data < Node->data){
+        return search(Node->left,data);
+    }else if(data > Node->data){
+        return search(Node->right,data);
+    }else if(data == Node->data){
+        cout<<"Value found"<<endl;
+    }
+    
+}
+
+void displayLeafNodes(node* Node){
+    if(Node == NULL){
+        return;
+    }else if(Node->left == NULL && Node->right == NULL){
+      cout<<Node->data<<" ";
+    }
+    displayLeafNodes(Node->left);
+    displayLeafNodes(Node->right);
+}
+
+void displayParentChild(node* Node){
+    if(Node->left != NULL && Node->right != NULL){
+        cout<<"Parent is "<<Node->data<<" and children are "<<(Node->left->data)<<" and "<<(Node->right->data)<<endl; 
+        displayParentChild(Node->left);
+        displayParentChild(Node->right);
+    }else if(Node->left != NULL && Node->right == NULL){
+        cout<< "Parent is "<<(Node->data)<<" and child is "<<(Node->left->data)<<endl;
+        displayParentChild(Node->left);
+    }else if(Node->left == NULL && Node->right != NULL){
+        cout<<"Parent is "<<(Node->data)+" and child is "<<(Node->right->data)<<"\n";
+        displayParentChild(Node->right);
+    }else{
+        return;
+    }
+    
+    
+}
+
+int displayDepth(node* Node){
+    if(Node == NULL){
+        return 0;
+    }
+    int leftDepth = displayDepth(Node->left);
+    int rightDepth = displayDepth(Node->right);
+    if(leftDepth > rightDepth){
+        return leftDepth+1;
+    }else{
+        return rightDepth+1;
+    }
+}
+
+struct node* deleteNode(node* Node,int data){
+    if(Node == NULL){
+        return NULL;
+    }
+    if(data < Node->data){
+        Node->left = deleteNode(Node->left,data);
+    }else if(data > Node->data){
+        Node->right = deleteNode(Node->right,data);
+    }else{
+        if(Node->left == NULL && Node->right == NULL){
+            free(Node);
+            return NULL;
+        }else if(Node->left == NULL || Node->right == NULL){
+            struct node* temp;
+            if(Node->left == NULL){
+                temp = Node->right;
+            }else{
+                temp = Node->left;
+            }
+            free(Node);
+            return temp;
+        }else{
+            struct node* temp = Node->right;
+            while(temp->left != NULL){
+                temp = temp->left;
+            }
+            Node->data = temp->data;
+            Node->right = deleteNode(Node->right,temp->data);
+        }
+    }
+    return Node;
+}
+
 void inorder(node* Node){
     if(Node == NULL){
         return;
@@ -48,16 +135,56 @@ int main(){
       int data;
       cin>>data;
       string choice;
+      int option;
       struct node* root = createNode(data);
-      do{
-          cout<<"Enter data for new node: "<<endl;
-          cin>>data;
-          insert(root,data); 
-          cout<<"Do you want to enter more data? (yes/no)"<<endl;
-        cin>>choice;
+      cout<<"Please enter choice of operation: "<<endl;
+      cout<<"1. Insert data"<<endl;
+      cout<<"2. Delete data"<<endl;
+      cout<<"3. Search data"<<endl;
+      cout<<"4. Display tree"<<endl;
+      cout<<"5. Display leaf nodes"<<endl;
+      cout<<"6. Display parent and child nodes"<<endl;
+      cout<<"7. Display depth of tree"<<endl;
+            do{
+        
+          cout<<"Enter option: "<<endl;
+          cin>>option;
+          switch(option){
+              case 1:
+                cout<<"Enter data for new node: "<<endl;
+                cin>>data;
+                insert(root,data);
+                break;
+              case 2:
+                cout<<"Enter data to delete: "<<endl;
+                cin>>data;
+                deleteNode(root,data);
+                break;
+              case 3:
+                cout<<"Enter data to search: "<<endl;
+                cin>>data;
+                search(root,data);
+                break;
+              case 4:
+                inorder(root);
+                break;
+              case 5:
+                cout<<"Leaf nodes are: "<<endl;
+                displayLeafNodes(root);
+                break;
+              case 6:
+                displayParentChild(root);
+                break;
+              case 7:
+                cout<<"Depth of tree is: "<<displayDepth(root)<<endl;
+                break;
+              default:
+                cout<<"Invalid option"<<endl;
+                break;
+          }
+          cout<<"Do you want to continue? (yes/no)"<<endl;
+          cin>>choice;
       }while(choice == "yes");
-
-      inorder(root);
 
 
 
